586434a373f78be55b4ceed147f0789d0727b9b6
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-07-31 14:17:54 +0200

    feat: make circuit use commitments

--- circuit/circuit.go
+++ circuit/circuit.go
@@ -1,14 +1,28 @@
 package circuit
 
-import "github.com/consensys/gnark/frontend"
+import (
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/std/math/emulated/emparams"
+)
 
 type Circuit struct {
-	X, Y frontend.Variable `gnark:",secret"`
-	Z    frontend.Variable `gnark:",public"`
+	X, Y       frontend.Variable                  `gnark:",secret"`
+	Z          frontend.Variable                  `gnark:",public"`
+	EmuX, EmuY emulated.Element[emparams.BN254Fr] `gnark:",secret"`
+	EmuZ       emulated.Element[emparams.BN254Fr] `gnark:",public"`
 }
 
 func (c *Circuit) Define(api frontend.API) error {
+	// check native multiplication
 	res := api.Mul(c.X, c.Y)
 	api.AssertIsEqual(res, c.Z)
+	// check emulated multiplication
+	f, err := emulated.NewField[emparams.BN254Fr](api)
+	if err != nil {
+		return err
+	}
+	res2 := f.Mul(&c.EmuX, &c.EmuY)
+	f.AssertIsEqual(res2, &c.EmuZ)
 	return nil
 }
